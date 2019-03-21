00e4f2dac62b2bc7f1232e6b88234af52db7988a
l <oliveira.mesquita@aluno.uece.br>
2019-03-21 18:56:04 -0300

    altered structure

--- src/modules/categories/CategoriesRoutes.js
+++ src/modules/categories/CategoriesRoutes.js
@@ -1,5 +1,5 @@
 const routes = require("express").Router();
-const CategoriesController = require("../controllers/categories/CategoriesController");
+const CategoriesController = require("./CategoriesController");
 
 routes.get("/getCategories", CategoriesController.getCategories);
 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
+
 // Rotas
-const CategoriesRoutes = require("./routes/CategoriesRoutes");
+const CategoriesRoutes = require("./modules/categories/CategoriesRoutes");
 
 const port = process.env.port || 3000;
+const domain = 'localhost'
 const app = express();
 
 app.use(CategoriesRoutes);
 
-app.listen(port, "localhost", () => {
-  console.log(`Server online na porta ${port}`);
+app.listen(port, domain, () => {
+  console.log(`Server online em http://${domain}:${port}`);
 });
